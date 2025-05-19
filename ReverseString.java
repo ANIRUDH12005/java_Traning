02e4de1c8f2300c541b0baea442a68e382f983b3
ANIRUDH12005 <aniyadav2005.1@gmail.com>
2025-05-19 20:06:06 +0530

    String assiment

---
+++ ReverseString.java
@@ -0,0 +1,13 @@
+public class ReverseString {
+    public static void main(String[] args) {
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        String reversed = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
+        }
+        System.out.println("Reversed string: " + reversed);
+        scanner.close();
+    }
+}
